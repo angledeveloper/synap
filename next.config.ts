5589b7461f5b759903c64245b4ed02f286b24f82
glitchnobody <siddhantvij001@gmail.com>
2025-07-14 15:34:41 +0530

    new config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["3.109.207.37"],
+    domains: ["3.109.207.37", "synapseaglobal.com"],
   },
 };
 
